2d76b526f680785cdf9453b28ed72af743ff2013
kartsridhar <contactkarthik34@gmail.com>
2018-02-07 12:20:50 +0000

    task 2, part 2

---
+++ src/Food.java
@@ -0,0 +1,13 @@
+public class Food {
+
+  //For a general animal
+  public String eaten(Animal animal) {
+    return "animal eats food";
+  }
+
+  //For a dog
+  public String eaten(Dog dog) {
+    return "dog eats food";
+  }
+
+}
